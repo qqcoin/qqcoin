ecd3c4f738b780930078a4f707987a1f8d51c5f5
Cryptuxe <cryptoconsulting@gmail.com>
2014-01-16 12:20:27 -0500

    Add Network Hashes Per Second
    Add Network Hashes Per Second

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -10,6 +10,38 @@
 
 using namespace json_spirit;
 using namespace std;
+ // uint64_t GetNetworkHashPS( int lookup )
+ //
+ //    WM - Function to estimate network hash rate.  Mostly lifted from
+ //    Litecoin and modified for YACoin.
+ //
+ //    Parameters: lookup (int) - How many blocks to look into the past.
+ //    Returns: Estimated YACoin network hash rate (uint64_t)
+ //
+ 
+ uint64_t GetNetworkHashPS( int lookup )
+ {
+     if( !pindexBest )
+         return 0;
+         
+     if( lookup < 0 )
+         lookup = 0;
+     
+     // If lookup is larger than chain, then set it to chain length.
+     if( lookup > pindexBest->nHeight )
+         lookup = pindexBest->nHeight;
+         
+     CBlockIndex *pindexPrev = pindexBest;
+     
+     for( int i = 0; i < lookup; ++i )
+         pindexPrev = pindexPrev->pprev;
+         
+     double timeDiff = pindexBest->GetBlockTime() - pindexPrev->GetBlockTime();
+     double timePerBlock = timeDiff / lookup;
+     
+     return (uint64_t)(((double)GetDifficulty() * pow(2.0, 32)) / timePerBlock);
+ }
+ 
 
 Value getgenerate(const Array& params, bool fHelp)
 {
@@ -77,11 +109,25 @@ Value getmininginfo(const Array& params, bool fHelp)
     obj.push_back(Pair("generate",      GetBoolArg("-gen")));
     obj.push_back(Pair("genproclimit",  (int)GetArg("-genproclimit", -1)));
     obj.push_back(Pair("hashespersec",  gethashespersec(params, false)));
+    obj.push_back(Pair("networkhashps", getnetworkhashps(params, false)));
     obj.push_back(Pair("pooledtx",      (uint64_t)mempool.size()));
     obj.push_back(Pair("testnet",       fTestNet));
+    // WM - Report current Proof-of-Work block reward.
+    
     return obj;
 }
 
+    // WM - Implementation of getnetworkhashps for YACoin
+  Value getnetworkhashps(const Array& params, bool fHelp)
+ {
+     if (fHelp || params.size() != 0)
+         throw runtime_error(
+             "getnetworkhashps\n"
+             "Returns an estimate of the VelcoityCoin network hash rate.");
+ 
+     return GetNetworkHashPS(params.size() > 0 ? params[0].get_int() : 120);
+ }
+ 
 Value getworkex(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() > 2)
